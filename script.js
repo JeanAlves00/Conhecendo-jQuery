313ac3c509110e2b9cfeb6af9b94243f56855042
JeanAlves00 <154983968+JeanAlves00@users.noreply.github.com>
2024-01-16 14:04:19 -0400

    implementando animação

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-    console.log($('header button'));
+    $('header button').click(function() {
+        $('form').slideDown();
+    })
+
+    $('#botao-cancelar').click(function (){
+        $('form').slideUp();
+    })
+
+
+    $('form').on('submit', function(e) {
+        console.log("submit")
+        e.preventDefault();
+    })
 })

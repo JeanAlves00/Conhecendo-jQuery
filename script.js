7dea92129a84e27b0171230cc27887c037af94c5
JeanAlves00 <154983968+JeanAlves00@users.noreply.github.com>
2024-01-17 14:10:40 -0400

    termino da da ativade

--- script.js
+++ script.js
@@ -9,7 +9,18 @@ $(document).ready(function() {
 
 
     $('form').on('submit', function(e) {
-        console.log("submit")
         e.preventDefault();
+        const enderecoDaNovaImagem = $('#indereço-imagem-nova').val();
+        const novoItem = $('<li style="display: none"></li>');
+        $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
+        $(`
+            <div class="overlay-imagem-link">
+                <a hrfer="${enderecoDaNovaImagem}" target="_blank" title="Ver Imagem em tamanho real">
+                    Ver imagem em tamanho real
+            </div>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(2000);
+        $('#indereço-imagem-nova').val('')
     })
 })
